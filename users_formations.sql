6d02458cf4fe58c599fafb4752b8b5059ef0a5d2
195152 <78798047+195152@users.noreply.github.com>
2021-12-20 21:36:52 +0100

    cs

---
+++ users_formations.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: users
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `formations`
+--
+
+DROP TABLE IF EXISTS `formations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `formations` (
+  `idformations` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) DEFAULT NULL,
+  `prix` varchar(50) DEFAULT NULL,
+  `Début` varchar(50) DEFAULT NULL,
+  `Fin` varchar(50) DEFAULT NULL,
+  `vide` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idformations`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `formations`
+--
+
+LOCK TABLES `formations` WRITE;
+/*!40000 ALTER TABLE `formations` DISABLE KEYS */;
+INSERT INTO `formations` VALUES (0000000001,'JAVA','200','06-12-2021','07-12-2021','s\'inscrire'),(0000000002,'HTML','150','09-12-2021','20-12-2021','s\'inscrire'),(0000000003,'Node Js','150','09-01-2022','11-01-2022','s\'inscrire');
+/*!40000 ALTER TABLE `formations` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-20 17:47:41
---
+++ users_inscription.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: users
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `inscription`
+--
+
+DROP TABLE IF EXISTS `inscription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inscription` (
+  `idinscription` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) DEFAULT NULL,
+  `idformation` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idinscription`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inscription`
+--
+
+LOCK TABLES `inscription` WRITE;
+/*!40000 ALTER TABLE `inscription` DISABLE KEYS */;
+INSERT INTO `inscription` VALUES (1,'ok','0000000001'),(2,'radhouane','0000000001'),(3,'radhouane','0000000002'),(4,'radhouane','0000000003'),(5,'TEXT','0000000001'),(6,'souha','0000000003'),(7,'souha','0000000001'),(8,'TEXT','0000000001'),(9,'TEXT','0000000001'),(10,'TEXT','0000000001'),(11,'TEXT','0000000001'),(12,'souh','0000000001'),(13,'TEXT','0000000001'),(14,'TEXT','0000000001'),(15,'TEXT','0000000002'),(16,'TEXT','0000000002'),(17,'TEXT','0000000001'),(18,'TEXT','0000000001'),(19,'TEXT','0000000001'),(20,'TEXT','0000000001');
+/*!40000 ALTER TABLE `inscription` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-20 17:47:41
